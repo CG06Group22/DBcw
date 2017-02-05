17d9ceb4448e89698271cbd8007b7949dbc8d917
Mei M <continue77777@gmail.com>
2017-02-05 16:16:39 +0000

    Update dbh.php

--- db/dbh.php
+++ db/dbh.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "logintest");
+$conn = mysqli_connect("ap-cdbr-azure-east-a.cloudapp.net", "ba5dd3c4a788eb", "5028ccf3", "logintest");
 
 if (!$conn){
     die("Connection failed: ".mysqli_connect_error());
