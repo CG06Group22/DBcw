c53787745305be6d2181b9d8b4fb85789330717b
chenenpei <chenenpei93@gmail.com>
2017-03-01 16:42:31 +0000

    Update search_test.php

--- sandbox/search_test.php
+++ sandbox/search_test.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-include '../db/dbh.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -67,25 +66,9 @@ include '../db/dbh.php';
 		<div class="container">
 			<?php
 			if(isset($_POST["submit"])){
-				//搜索框输入的文字
-				$searchs = $_POST['search'];
-				//勾选的 checkbox
-				$checkbox_select = $_POST['checkbox'];
-
-				if ($checkbox_select == "friends"){
-					//当 checkbox 勾选的是 friends 时
-					$sql="SELECT * FROM blog
-					WHERE title like ‘%$search%’";
-					$result = mysqli_query($conn, $sql);
-				} else ($checkbox_select =="articles"){
-					//当 checkbox 勾选的是  articles 时
-					$sql="SELECT * FROM users WHERE firstName OR lastName LIKE '%$search%'";
-					$result = mysqli_query($conn, $sql);
-				}
-			}
-			echo "<pre>";
-			print_r($result);
-			echo "</pre>";
+			echo $_POST["search"];
+			echo $_POST["checkbox"];
+			}	
 			?>
 			<!-- <div id="friends-results">
 				<h3>Friends Results</h3>
