afb95303595fa837dd0dec76e4eee51f65c62d50
chenenpei <chenenpei93@gmail.com>
2017-03-01 15:04:29 +0000

    Add files via upload

--- sandbox/search.php
+++ sandbox/search.php
@@ -1,5 +1,25 @@
 <?php
 session_start();
+
+include '../db/dbh.php';
+
+//搜索框输入的文字
+$searchs = $_POST['search'];
+//勾选的 checkbox
+$checkbox_select = $_POST['checkbox'];
+
+if ($checkbox_select == "friends"){
+//当 checkbox 勾选的是 friends 时
+	$sql="SELECT * FROM blog
+	WHERE title like ‘%$search%’
+	";
+	$result = mysqli_query($conn, $sql);
+} else ($checkbox_select =="articles"){
+//当 checkbox 勾选的是  articles 时
+	$sql="SELECT * FROM users WHERE firstName OR lastName LIKE '%$search%'
+	";
+	$result = mysqli_query($conn, $sql);
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -61,7 +81,9 @@ session_start();
 
 		<section id="searchlist">
 			<div class="container">
-				
+				<?php 
+				echo $result;
+				?>
 			<!-- <div id="friends-results">
 				<h3>Friends Results</h3>
 				<div class="list-group">
