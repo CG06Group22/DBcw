376ff4d1737d4f69ca564c049aa7e47e22aff68e
chenenpei <chenenpei93@gmail.com>
2017-03-02 00:40:54 +0000

    Update search_test.php

--- sandbox/search_test.php
+++ sandbox/search_test.php
@@ -73,7 +73,7 @@ session_start();
 					while($row=mysql_fetch_array($result)){ 
 						$firstName = $row['firstName']; 
 						$lastName = $row['lastName'];
-						$fullNmae .= $firstName ." ".$lastName;
+						$fullName .= $firstName ." ".$lastName;
 						echo $fullName;
 						echo "<hr>";
 					}
