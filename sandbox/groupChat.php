ca91fa1ae2fd063d8cdd1412a973383bb66e566a
Mei M <continue77777@gmail.com>
2017-03-05 01:00:30 +0000

    Add files via upload

--- sandbox/groupChat.php
+++ sandbox/groupChat.php
@@ -1,6 +1,13 @@
 <?php 
 include '../db/dbh.php'; 
 ?>
+<?php 
+  // session_start();
+  $groupid = $_GET['id'];
+  $groupName = $_GET['name']
+  $query = "SELECT * FROM messages WHERE gid = '$groupid' ORDER BY messageid DESC";
+  $messages = mysqli_query($conn, $query);
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -17,6 +24,43 @@ include '../db/dbh.php';
     <?php
       include("../component/header.php");
     ?>
-    
+    <div id="container">
+      <header>
+        <a href="../sandbox/chat.php">
+        Back
+        </a>
+        <h1><?php echo $groupName; ?></h1>
+      </header>
+      <div id="messages" class="container">
+        <ul>
+          <?php while($row = mysqli_fetch_assoc($messages)) : ?>
+            <li class="message">
+              <span><?php echo $row['sendTime']; ?> - </span>
+              <strong>
+              <?php
+                $que = "SELECT firstName, lastName FROM users WHERE uid = '$row['uid']'";
+                $name = mysqli_query($conn, $que);
+                $nameRow = mysqli_fetch_assoc($name);
+                $fullName = $nameRow['firstName'] ." ".$nameRow['lastName'];
+                echo $fullName;
+                $content = $row['content'];
+              ?>                
+              </strong>
+              : <?php echo $content; ?>
+            </li>     
+          <?php endwhile; ?>
+        </ul>
+      </div>
+      <div id="input">
+        <?php if (isset($_GET['error'])) : ?>
+          <div class="error"><?php echo $_GET['error']; ?></div>
+        <?php endif; ?>
+        <form method="post" action="includes/process.php">
+          <!-- <input type="text" id="user" name="user" placeholder="Enter Your Name"/> -->
+          <input type="text" id="newmessage" name="message" placeholder="Enter A Message"/>
+          <input id="show-btn" type="submit" name="submit" value="Send"/>
+        </form>
+      </div>
+    </div>
   </body>
 </html>
