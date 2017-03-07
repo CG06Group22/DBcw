0831e15b7150623f17d3d35f89d0fd1f03ebd11b
HighwayZ <asino1990@live.com>
2017-03-07 19:46:34 +0000

    chat bug

--- sandbox/chat.php
+++ sandbox/chat.php
@@ -5,139 +5,145 @@ include '../db/dbh.php';
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>FakeBook | Chat</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>FakeBook | Chat</title>
 
-  <!-- Bootstrap core CSS -->
-  <link href="../css/bootstrap.min.css" rel="stylesheet">
-  <link href="../css/style.css" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
 </head>
 
 <body>
-  <!-- navbar -->
-  <?php
-    include("../component/header.php");
-  ?>
+<!-- navbar -->
+<?php
+include("../component/header.php");
+?>
 
 <section id="user">
     <div class="container">
-      <a href="profile.php"><h1 class="page-header">
-      <?php
-                      if (isset($_SESSION['first'])){
-                          echo $_SESSION['first'];
-        echo " ";    
-                          echo $_SESSION['last'];
-                     };
-      ?>
-      </h1></a>
+        <a href="profile.php"><h1 class="page-header">
+                <?php
+                if (isset($_SESSION['first'])){
+                    echo $_SESSION['first'];
+                    echo " ";
+                    echo $_SESSION['last'];
+                };
+                ?>
+            </h1></a>
     </div>
 </section>
 
 <section id="group-list">
-  <div class="container">
-    <h2 class="text-primary">Group List</h2>
-    <div class="list-group">
-      <?php
-        $uid=$_SESSION['uid'];
-        $sql = "SELECT gid FROM usersgroup WHERE uid = '$uid'";
-        $result = mysqli_query($conn, $sql);
-        $count = mysqli_num_rows($result);
-        if(!$count>0){
-          echo "You don't have any group yet.";
-        }else{
-          while($row=mysqli_fetch_array($result)){
-            $gid = $row['gid'];
-            $sql2 = "SELECT groupName FROM groups WHERE gid = '$gid'";
-            $result2 = mysqli_query($conn, $sql2);
-            if (!$rowGroupNames = mysqli_fetch_assoc($result2)){
-                echo "Can't find group.";
-            } else{
-              $groupName = $rowGroupNames['groupName'];
-            }
-      ?>
-            <div class='list-group-item'>
-                <div class = "panel-group" id="buttons-div">
-                    <div class = "panel panel-default">
-                      <?php
-                      echo "<a href='groupChat.php?$gid'>";
-                      echo $groupName;
-                      echo "</a>";
-                      ?>
-<!--                       <hr> -->
-                      <div class = "panel-heading">
-                        <?php echo "<a class='btn btn-primary' data-toggle='collapse' href='#$gid'>Detail
-                        </a>"; ?>
-                        <button class="btn btn-danger">Leave</button>
-                      </div>
-                    <!-- （panel-collapse） -->
-                    <?php echo "<div class='panel-collapse collapse' id='$gid'>"; ?>
-                      <ul class="list-group">
-                      <?php
-                      $target = $_SESSION['email'];
-                      $friend = "friend";
-                      $sqlfriends = "SELECT guestUserID FROM relationship WHERE relationship = '$friend' AND hostUserID = '$target'";
-                      $resultfriends = mysqli_query($conn, $sqlfriends);
-                      $countfriends = mysqli_num_rows($resultfriends);
-                      if(!$countfriends>0){
+    <div class="container">
+        <h2 class="text-primary">Group List</h2>
+        <ul class="list-group">
+            <?php
+            $uid=$_SESSION['uid'];
+            $sql = "SELECT gid FROM usersgroup WHERE uid = '$uid'";
+            $result = mysqli_query($conn, $sql);
+            $count = mysqli_num_rows($result);
+            if(!$count>0){
+                echo "You don't have any group yet.";
+            }else{
+            while($row=mysqli_fetch_array($result)) {
+                $gid = $row['gid'];
+                $sql2 = "SELECT groupName FROM groups WHERE gid = '$gid'";
+                $result2 = mysqli_query($conn, $sql2);
+                if (!$row = mysqli_fetch_assoc($result2)) {
+                    echo "Can't find group.";
+                } else {
+                    $groupName = $row['groupName'];
+                }
+                echo "<li class='list-group-item'><a href='groupChat.php?$gid'>$groupName</a>";
+
+                ?>
+
+                <button class='btn btn-primary' data-toggle='modal' data-target='#myModal2'>Detail
+                </button>
+                <button class="btn btn-danger">Leave</button>
+                <!-- （Modal） -->
+                <div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+                    </button>
+                    Add members to <?php echo $groupName; ?>
+                </div>
+                <div class="modal-body">
+                <div id='friend'>
+                <h3>Friends</h3>
+                <ul class="list-group">
+
+                    <?php
+                    $target = $_SESSION['email'];
+                    $friend = "friend";
+                    $sql5 = "SELECT guestUserID FROM relationship WHERE relationship = '$friend' AND hostUserID = '$target'";
+                    $result5 = mysqli_query($conn, $sql5);
+                    $count = mysqli_num_rows($result5);
+                    if (!$count > 0) {
                         echo "Your friend list is empty";
-                      }else{
-                        while($rowfriends = mysqli_fetch_array($resultfriends)){
-                            $guestUserID = $rowfriends['guestUserID'];
-                            $sqlGuestNames = "SELECT firstName, lastName FROM users WHERE email = '$guestUserID'";
-                            $resultGuestNames = mysqli_query($conn, $sqlGuestNames);
-                            if (!$rowGuestNames = mysqli_fetch_assoc($resultGuestNames)){
+                    }
+                    else{
+
+                        while($row=mysqli_fetch_array($result5)) {
+                            $guestUserID = $row['guestUserID'];
+                            $sql6 = "SELECT firstName, lastName FROM users WHERE email = '$guestUserID'";
+                            $result6 = mysqli_query($conn, $sql6);
+                            if (!$row = mysqli_fetch_assoc($result6)) {
                                 echo "Can't find user.";
                             } else {
-                                $firstName = $rowGuestNames['firstName'];
-                                $lastName = $rowGuestNames['lastName'];
-                                $fullName = $firstName ." ".$lastName;
-
-                                echo "<li class='list-group-item'><a href='others-profile.php?$guestUserID'>";
-                                echo $fullName;
-
-                                $sqlIfIn = "SELECT gid FROM usersgroup WHERE gid = '$gid' AND uid = '$guestUserID'";
-                                $resultIfIn = mysqli_query($conn, $sqlIfIn);
-                                if (!$rowadd = mysqli_fetch_assoc($resultIfIn)){
-                                    echo "<a href='../includes/addToGroup.php?u=$guestUserID&g=$gid'>Add</a>";
-                                }
-                                echo "</li>";
+                                $firstName = $row['firstName'];
+                                $lastName = $row['lastName'];
+                            }
+                            $fullName = $firstName . " " . $lastName;
+                            ?>
+                            <li class='list-group-item'><a href='others-profile.php?<?php echo $guestUserID ?>'></a>
+                              <?php
+                              echo $fullName;
+                              $sqlIfIn = "SELECT gid FROM usersgroup WHERE gid = '$gid' AND uid = '$guestUserID'";
+                              $resultIfIn = mysqli_query($conn, $sqlIfIn);
+                            if (!$rowadd = mysqli_fetch_assoc($resultIfIn)){
+                                ?>
+                                <a href='../includes/addToGroup.php?u=$guestUserID&g=$gid'>Add</a></li>
+                                <?php
                             }
                         }
-                      }
+                    }
+                    ?>
+                </ul>
+                </div>
 
-                      ?>
-                      </ul>
-                    <?php echo "</div>"; ?>
-                    <!-- （panel-collapse-close） -->
+                            </div>
+                        </div><!-- /.modal-content -->
+                    </div><!-- /.modal -->
+                </div>
 
-                      
-                    </div> <!-- panel panel-default -->
-                </div> <!-- panel-group -->
-            </div> <!-- list group item -->
-      <?php
-          }
-        }
-      ?>
-    </div> <!-- list group -->
-  </div> <!-- container -->
+
+    <?php
+    }
+    }
+    ?>
+        </ul>
+    </ul>
+    </div>
 </section>
 
 <footer>
-  <div class="container-fluid">
-    <hr>
-    <p class="text-center">FakeBook, Copyright &copy; 2017
-    </p>
-  </div> 
+    <div class="container-fluid">
+        <hr>
+        <p class="text-center">FakeBook, Copyright &copy; 2017
+        </p>
+    </div>
 </footer>
 
 
 <script
-src="http://code.jquery.com/jquery-3.1.1.js"
+        src="http://code.jquery.com/jquery-3.1.1.js"
 ></script>
 <script src="../js/bootstrap.min.js"></script>
 <script src="../js/main.js"></script>
 </body>
 </html>
-
