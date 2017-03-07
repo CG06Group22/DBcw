969ca5a4b0e52156d59647bcc037fa5b266eb702
MingxuanM <continue77777@gmail.com>
2017-03-07 15:05:41 +0000

    no message

--- includes/addToGroup.php
+++ includes/addToGroup.php
@@ -1,16 +1,20 @@
 <?php
 include '../db/dbh.php';
-$uemail = htmlspecialchars($_GET["u"]);
-$gid = htmlspecialchars($_GET["g"])
 
-$sqlid = "SELECT uid FROM users WHERE email = $uemail";
-$uidresult = mysqli_query($conn, $sqlid);
-if (!$uidrow = mysqli_fetch_assoc($uidresult)){
-                $uid = $uidrow['uid'];
-            }
+if(isset($_GET['u']) && !empty($_GET['u']) AND isset($_GET['g']) && !empty($_GET['g'])){
+	$uemail = $_GET['u'];
+	$gid = $_GET['g'];
+	$sqlid = "SELECT uid FROM users WHERE email = $uemail";
+	$uidresult = mysqli_query($conn, $sqlid);
+	if (!$uidrow = mysqli_fetch_assoc($uidresult)){
+        $uid = $uidrow['uid'];
+    }
 
-$sql = "INSERT INTO usersgroup ( uid, gid ) VALUES ('$uid', '$gid')";
-$result = mysqli_query($conn, $sql);
+	$sql = "INSERT INTO usersgroup ( uid, gid ) VALUES ('$uid', '$gid')";
+	$result = mysqli_query($conn, $sql);
+}
+
+
 
 header("Location: ../sandbox/chat.php");
 
