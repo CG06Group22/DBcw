7b919a7be03e08518544560899be89517989e390
MingxuanM <continue77777@gmail.com>
2017-03-08 17:31:23 +0000

    add jquery

--- sandbox/chat.php
+++ sandbox/chat.php
@@ -13,6 +13,10 @@ include '../db/dbh.php';
   <!-- Bootstrap core CSS -->
   <link href="../css/bootstrap.min.css" rel="stylesheet">
   <link href="../css/style.css" rel="stylesheet">
+  <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.fix.js"></script>
+
+
 </head>
 
 <body>
