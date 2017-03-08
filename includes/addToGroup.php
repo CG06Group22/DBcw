25bc81c50e4824dc574b55e898bddb4c3c77e0d6
MingxuanM <continue77777@gmail.com>
2017-03-08 13:31:03 +0000

    try $_REQUEST rather than $_GET

--- includes/addToGroup.php
+++ includes/addToGroup.php
@@ -1,9 +1,10 @@
 <?php
+// from: <a href='../includes/addToGroup.php?u=$guestUserID&g=$gid'>
 include '../db/dbh.php';
 
-if(isset($_GET['u']) && !empty($_GET['u']) AND isset($_GET['g']) && !empty($_GET['g'])){
-	$uemail = $_GET['u'];
-	$gid = $_GET['g'];
+if(isset($_REQUEST['u']) && !empty($_REQUEST['u']) AND isset($_REQUEST['g']) && !empty($_REQUEST['g'])){
+	$uemail = $_REQUEST['u'];
+	$gid = $_REQUEST['g'];
 	$sqlid = "SELECT uid FROM users WHERE email = $uemail";
 	$uidresult = mysqli_query($conn, $sqlid);
 	if (!$uidrow = mysqli_fetch_assoc($uidresult)){
@@ -13,8 +14,6 @@ if(isset($_GET['u']) && !empty($_GET['u']) AND isset($_GET['g']) && !empty($_GET
 	$sql = "INSERT INTO usersgroup ( uid, gid ) VALUES ('$uid', '$gid')";
 	$result = mysqli_query($conn, $sql);
 }
-
-
 
 header("Location: ../sandbox/chat.php");
 
