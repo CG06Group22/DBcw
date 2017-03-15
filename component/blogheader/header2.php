915639574d77c5dd1f608760d8883f365803cedc
chenenpei <chenenpei93@gmail.com>
2017-03-15 16:00:21 +0000

    Update header2.php

--- component/blogheader/header2.php
+++ component/blogheader/header2.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
         <div class="navbar-header">
-            <h3 style="color:#337ab7">FakeBook</h3>
+            <h3 style="color:#fff">FakeBook</h3>
         </div>
         <ul class="nav navbar-nav navbar-right">
             <li><a href="../../sandbox/profile.php">Home</a></li>
