c62dc5d72c79f1be80984036215ff3c41442f26e
chenenpei <chenenpei93@gmail.com>
2017-03-16 14:32:09 +0000

    Update header.php

--- component/header.php
+++ component/header.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
         <div class="navbar-header">
-            <h3 style="color:#fff">FakeBook</h3>
+            <h3 style="margin-left:5px, color:#fff">FakeBook</h3>
         </div>
         <ul class="nav navbar-nav navbar-right">
             <li><a href="../sandbox/profile.php">Home</a></li>
